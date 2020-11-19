0ee78e46b37415427b0a991ebd30ff8bbd9198de
Alex Vak <mamorunokitsune@gmail.com>
2020-11-19 15:27:40 +0200

    Lab2 v1(NaN  problem)

---
+++ ЧМІ_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ЧМІ_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+
+
+                Console.WriteLine("Enter dimension of the matrix:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                double[,] a = new double[n, n];
+                double[] b = new double[n];
+                double[] x = new double[n];//початкові наближення
+                for (int i = 0; i < n; i++)
+                {
+                    x[i] = 0;
+                }
+                Console.WriteLine("Write down your matrix");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.WriteLine("Enter the element from position[" + (i + 1) + ", " + (j + 1) + "]:");
+                        a[i, j] = Convert.ToDouble(Console.ReadLine());
+                    }
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Enter the value from position [" + (i + 1) + "]:");
+                    b[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Seidel myMatrix = new Seidel(a, b, 500, n, x);
+                myMatrix.GetSeidel();
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("X" + (i + 1) + " = " + myMatrix.roots[i]);
+                }
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ ЧМІ_2/Seidel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ЧМІ_2
+{
+    public class Seidel
+    {
+        static double eps = 0.01;
+        int n, itrCounter, N; // розмірність матриці, кількість ітерацій, допустима кількість ітерацій
+        double s, Xi, diff = 1;
+        double[,] matrix;
+        double[] value;
+        public double[] roots;
+
+        public Seidel(double[,] matrix, double[] value, int N, int n, double[] roots)
+        {
+            this.matrix = matrix;
+            this.N = N;
+            this.value = value;
+            this.n = n;
+            this.roots = roots;
+        }
+        public void GetSeidel()
+        {
+            itrCounter = 0;
+            while (itrCounter<=N && diff>=eps)
+            {
+                itrCounter++;
+                for (int i = 0; i < n; i++)
+                {
+                    s = 0;
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i != j) 
+                        {
+                            s += matrix[i, j] * roots[j];
+                        }
+                    }
+                    Xi = (value[i] - s) / matrix[i, i];
+                    diff = Math.Abs(Xi - roots[i]);
+                    roots[i] = Xi;
+                }
+            }
+        }
+    }
+}
