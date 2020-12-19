b488000c0b266d324adcea414468a9b1d42c3947
Alex Vak <mamorunokitsune@gmail.com>
2020-12-20 01:27:37 +0200

    FINALY!!!!! ITS ALIVE!!!!!!!!!

--- ЧМІ_2/Program.cs
+++ ЧМІ_2/Program.cs
@@ -8,24 +8,31 @@ namespace ЧМІ_2
         {
             void Test()
             {
-                double[,] a = {{ 20, 2, 3, 7 }, { 1, 12, -2, -5 }, { 5, -3, 13, 0 }, { 0, 0, -1, 15 }};
-                double[] b = { 5, 4, -3, 7 };
-                double[] x = { 0, 0, 0, 0};
-                int n = 4;
+                //double[,] a2 = { { 4, 2, 1 } , { 3, 4.5, 4 }, { 2, 1, 5 } };
+                double[] b2 = { 20, 25, 17 };
+                double[] x2 = { 0, 0, 0 };
+                int n2 = 3;
+                double[][] array =
+                {
+                    new double[] { 2, 1, 5 },
+                    new double[] { 3, 4.5, 4 },
+                    new double[] { 4, 2, 1 }
+                };
 
-                Seidel myMatrix = new Seidel(a, b, 100, n, x);
-                for (int i = 0; i < n; i++)
+                Seidel myMatrix = new Seidel(array, b2, 100, n2, x2);
+                for (int i = 0; i < n2; i++)
                 {
                     Console.WriteLine("X" + (i + 1) + " = " + myMatrix.roots[i]);
                 }
 
                 Console.WriteLine(" ");
-                x[0] = 409.0 / 108.0;
-                x[1] = 89.0 / 54.0;
-                x[2] = 14.0 / 9.0;
-                for (int i = 0; i < n; i++)
+                Console.WriteLine("Correct answer");
+                x2[0] = 409.0 / 108.0;
+                x2[1] = 89.0 / 54.0;
+                x2[2] = 14.0 / 9.0;
+                for (int i = 0; i < n2; i++)
                 {
-                    Console.WriteLine(x[i]); 
+                    Console.WriteLine(x2[i]); 
                 }
             }            
 
@@ -35,7 +42,7 @@ namespace ЧМІ_2
                 
                 Console.WriteLine("Enter dimension of the matrix:");
                 int n = Convert.ToInt32(Console.ReadLine());
-                double[,] a = new double[n, n];
+                double[][] a = new double[n][];
                 double[] b = new double[n];
                 double[] x = new double[n];//початкові наближення
                 for (int i = 0; i < n; i++)
@@ -45,10 +52,11 @@ namespace ЧМІ_2
                 Console.WriteLine("Write down your matrix");
                 for (int i = 0; i < n; i++)
                 {
+                    a[i] = new double[n];
                     for (int j = 0; j < n; j++)
-                    {
+                    {                        
                         Console.WriteLine("Enter the element from position[" + (i + 1) + ", " + (j + 1) + "]:");
-                        a[i, j] = Convert.ToDouble(Console.ReadLine());
+                        a[i][j] = Convert.ToDouble(Console.ReadLine());
                     }
                 }
                 for (int i = 0; i < n; i++)
@@ -57,10 +65,10 @@ namespace ЧМІ_2
                     b[i] = Convert.ToDouble(Console.ReadLine());
                 }
 
-                Seidel myMatrix = new Seidel(a, b, 50, n, x);
+                Seidel mymatrix = new Seidel(a, b, 50, n, x);
                 for (int i = 0; i < n; i++)
                 {
-                    Console.WriteLine("X" + (i + 1) + " = " + myMatrix.roots[i]);
+                    Console.WriteLine("x" + (i + 1) + " = " + mymatrix.roots[i]);
                 }
 
             } while (Console.ReadKey().Key != ConsoleKey.Escape);
--- ЧМІ_2/Seidel.cs
+++ ЧМІ_2/Seidel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace ЧМІ_2
 {
@@ -9,28 +10,62 @@ namespace ЧМІ_2
         static double eps = 0.01;
         static double lambda = .5;
         int n, maxItr; 
-        double[,] matrix;
+        double[][] matrix;
         double[] bValue;
         public double[] roots;
+        double[][] tmpArr;
+        double[][] colls;
 
-        public Seidel(double[,] matrix, double[] bValue, int maxItr, int n, double[] roots)
+        public Seidel(double[][] matrix, double[] bValue, int maxItr, int n, double[] roots)
         {
             this.matrix = matrix;
             this.maxItr = maxItr;
             this.bValue = bValue;
             this.n = n;
             this.roots = roots;
+
+            tmpArr = new double[n][];
+            colls = new double[n][];
+
             GetSeidel();
         }
-        private void GetSeidel()
+
+        void GetColls()
+        {            
+            for (int i = 0; i < n; i++)
+            {
+                colls[i] = new double[n];
+                tmpArr[i] = new double[n];
+                tmpArr[i] = matrix[i];
+                for (int j = 0; j < n; j++)
+                {
+                    colls[i][j] = matrix[j][i];
+                }
+            }
+        }
+
+        void TransformMatrix()
         {
+            GetColls();
+            int maxIndex;
+
+            for (int i = 0; i < n; i++)
+            {
+                maxIndex = Array.IndexOf(colls[i], colls[i].Max());
+                matrix[i] = tmpArr[maxIndex];
+            }
+        }
+        void GetSeidel()
+        {
+            TransformMatrix();
+
             //Division by the diagonal element to reduce calculation
             for (int i = 0; i < n; i++)
             {
-                double d = matrix[i, i];
+                double d = matrix[i][i];
                 for (int j = 0; j < n; j++)
                 {
-                    matrix[i, j] = matrix[i, j] / d;
+                    matrix[i][j] = matrix[i][j] / d;
                 }
                 bValue[i] = bValue[i] / d;
             }
@@ -43,7 +78,7 @@ namespace ЧМІ_2
                 {
                     if (i != j)
                     {
-                        sum -= (matrix[i, j] * roots[j]);
+                        sum -= (matrix[i][j] * roots[j]);
                     }
                 }
                 roots[i] = sum;
@@ -61,7 +96,7 @@ namespace ЧМІ_2
                     for (int j = 0; j < n; j++)
                     {
                         if (i != j)
-                            sum -= (matrix[i, j] * roots[j]);
+                            sum -= (matrix[i][j] * roots[j]);
                     }
 
                     roots[i] = lambda * sum + (1 - lambda) * old;
